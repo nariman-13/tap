8627bf27a0d4458c619032d75a0541dc1e65ae21
nariman-13 <80679172+nariman-13@users.noreply.github.com>
2023-11-24 14:08:20 +0100

    Update HelloController.java
    playing around with java code and deployment

--- tanzu-java-web-app/src/main/java/com/example/springboot/HelloController.java
+++ tanzu-java-web-app/src/main/java/com/example/springboot/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
 	@RequestMapping("/")
 	public String index() {
-		return "Greetings from Spring Boot + Nariman + evoila!";
+		return "Greetings from Spring Boot + Nariman + evoila! + testing";
 	}
 
 }
