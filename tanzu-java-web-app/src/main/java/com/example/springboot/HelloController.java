8e8bc2202d552360e38c0c9433bfffa482b8dd5a
nariman-13 <80679172+nariman-13@users.noreply.github.com>
2023-11-24 12:09:10 +0100

    Update HelloController.java
    playing around with the code

--- tanzu-java-web-app/src/main/java/com/example/springboot/HelloController.java
+++ tanzu-java-web-app/src/main/java/com/example/springboot/HelloController.java
@@ -1,14 +1,32 @@
 package com.example.springboot;
 
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+import java.time.Period;
 
 @RestController
 public class HelloController {
 
-	@RequestMapping("/")
-	public String index() {
-		return "Greetings from Spring Boot + Test!";
-	}
+    @GetMapping("/")
+    public String index() {
+        return "Greetings from Spring Boot + Test!";
+    }
 
+    @PostMapping("/calculateAge")
+    public String calculateAge(@RequestParam String name, @RequestParam String dob) {
+        // Parse the input string into a LocalDate object
+        LocalDate dobDate = LocalDate.parse(dob);
+
+        // Calculate the age
+        Period age = calculateAge(dobDate, LocalDate.now());
+
+        // Display the result
+        return "Hello, " + name + "! Your age is: " + age.getYears() + " years, " + age.getMonths() + " months, and " + age.getDays() + " days.";
+    }
+
+    // Function to calculate age
+    private static Period calculateAge(LocalDate dob, LocalDate currentDate) {
+        return Period.between(dob, currentDate);
+    }
 }
