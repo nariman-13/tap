e511bf991c3c9e742ad54b38a90a330495c2f8be
nariman-13 <80679172+nariman-13@users.noreply.github.com>
2023-12-16 17:26:16 +0100

    Update HelloController.java

--- tanzu-java-web-app/src/main/java/com/example/springboot/HelloController.java
+++ tanzu-java-web-app/src/main/java/com/example/springboot/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
 	@RequestMapping("/")
 	public String index() {
-		return "Greetings from Spring Boot + Second Test !!!";
+		return "Greetings from Spring Boot + Third Test !!!";
 	}
 
 }
